c1b0a2167d7d475a2cf7d1be14c6302c27b1f219
Bugsnumeric <alexandru2004petru@gmail.com>
2026-04-27 09:35:54 +0300

    1stcommit

--- laboratories/cicd-documentation/tree.py
+++ laboratories/cicd-documentation/tree.py
@@ -1,5 +1,8 @@
 from node import Node
 
+inorder_list = []
+preorder_list = []
+postorder_list = []
 
 class Tree:
     """ Tree class for binary tree """
@@ -64,22 +67,79 @@ class Tree:
     def deleteTree(self):
         self.root = None
 
-    def printTree(self):
+    def printTreeInorder(self):
         if self.root is not None:
             self._printInorderTree(self.root)
+        print("Inorder: ", inorder_list)
+
+    def printTreePreorder(self):
+        if self.root is not None:
+            self._printPreorderTree(self.root)
+        print("Preorder: ", preorder_list)
+
+    def printTreePostorder(self):
+        if self.root is not None:
+            self._printPostorderTree(self.root)
+        print("Postorder: ", postorder_list)
 
     def _printInorderTree(self, node):
         if node is not None:
             self._printInorderTree(node.left)
-            print(str(node.data) + ' ')
+            inorder_list.append(node.data)
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
         # TODO
-        pass
+        if node is not None:
+            preorder_list.append(node.data)
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
 
     def _printPostorderTree(self, node):
         # TODO
-        pass
+        if node is not None:
+            self._printPostorderTree(node.left)
+            self._printPostorderTree(node.right)
+            postorder_list.append(node.data)
 
+def setup_tree():
+    tree = Tree()
+    tree.add(3)
+    tree.add(4)
+    tree.add(0)
+    tree.add(8)
+    tree.add(2)
 
+    inorder_list.clear()
+    preorder_list.clear()
+    postorder_list.clear()
+
+    return tree
+
+def test_inorder():
+    tree = setup_tree()
+    tree.printTreeInorder()
+    assert inorder_list == [0, 2, 3, 4, 8]
+
+def test_preorder():
+    tree = setup_tree()
+    tree.printTreePreorder()
+    assert preorder_list == [3, 0, 2, 4, 8]
+
+def test_postoder():
+    tree = setup_tree()
+    tree.printTreePostorder()
+    assert postorder_list == [2, 0, 8, 4, 3]
+
+def test_find_existing():
+    tree = setup_tree()
+    node = tree.find(2)
+
+    assert node is not None
+    assert node.data == 2
+
+def test_find_non_existing():
+    tree = setup_tree()
+    node = tree.find(10)
+
+    assert node is None
